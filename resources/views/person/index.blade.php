339e0a5b0788d4ed577e507396c977be4ed585ee
MohammadMahdi-khajeloo <mohammad.khajeloo.37@gmail.com>
2023-01-28 17:11:34 +0330

    edit index page

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>slam</title>
+    <title>todo list</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
-<h1>slam</h1>
+<h1>This page shows a list of users</h1>
 <hr>
 <div class="container">
     <div class="col-12">
